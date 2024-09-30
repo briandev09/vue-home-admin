eb07bab31312e9b4978dccb372a70b0466051cde
Brian Guzman <brianguzmandev@gmail.com>
2024-09-30 02:03:33 -0700

    [fix/api] fixed-add-upate-api

--- src/stores/users.ts
+++ src/stores/users.ts
@@ -12,6 +12,7 @@ export const useUserStore = defineStore('users', {
     }),
     actions: {
         async fetchUsers() {
+
             await axios.get(`${apiUrl}/users`)
                 .then((result) => {
                     this.users = result?.data
@@ -21,48 +22,39 @@ export const useUserStore = defineStore('users', {
                 })
         },
         async addUsers(firstName: string, secondName: string) {
-            //add new user to user state in the store 
-            const currentDate = this.getCurrentDate();
-            this.users.push({
-                id: (this.users.length + 1).toString(),
-                firstName: firstName,
-                lastName: secondName,
-                createdAt:currentDate
-          })
             // add new user to database
-            axios.post(`${apiUrl}/users`, {
-                firstName:firstName,
-                lastName:secondName
+            await axios.post(`${apiUrl}/users`, {
+                firstName: firstName,
+                lastName: secondName
             }).then((result) => {
                 if (result?.data) {
                     alert("Add new user successfully!")
-
+                    console.log(result?.data);
+                    this.users.push(result?.data)
                 }
             }).catch((error) => {
                 alert(error.message)
             })
         },
-       async updateUsers(id:string,firstName:string,secondName:string) {
-        const currentDate = this.getCurrentDate();
-            const newUpdateUser ={
-                id: id,
-                firstName: firstName,
-                lastName: secondName,
-                createdAt:this.getCustomDate(currentDate)
-            }
-            const index = this.users.findIndex((user:User) => user.id === id)
-            if(index !== -1){
-                 
-                this.users[index] = {...this.users[index],...newUpdateUser}
-            }
+        async updateUsers(id: string, firstName: string, secondName: string) {
+            await axios.put(`${apiUrl}/users/${id}`,{
+                firstName:firstName,
+                lastName:secondName
+            }).then((result)=>{
+                if(result?.data){
+                    alert("Updated user successfully!")
+                    const index = this.users.findIndex((user: User) => user.id === id)
+                    if (index !== -1) {
+        
+                        this.users[index] = { ...this.users[index], ...result?.data }
+                    }
+                }
 
-            // await axios.put(`${apiUrl}/users/id`,{}).then((result)=>{
-
-            // }).catch((error)=>{
-            //     alert(error.message)
-            // })
+            }).catch((error)=>{
+                alert(error.message)
+            })
         },
-        getCustomDate(_date:string) {
+        getCustomDate(_date: string) {
             const date = new Date(_date)
             return date.toLocaleString('en-US', {
                 year: 'numeric',
@@ -73,7 +65,7 @@ export const useUserStore = defineStore('users', {
                 hour12: true
             })
         },
-        getCurrentDate(){
+        getCurrentDate() {
             const currentDate = new Date()
             const isoDateString = currentDate.toDateString()
             return isoDateString
